f621ec0ec20d540ee29b182b8712edca936a125f
김동욱 <email@example.com>
2023-03-16 10:50:53 +0900

    22

--- users/views.py
+++ users/views.py
@@ -137,7 +137,7 @@ class GithubLogIn(APIView):
         try:
             code = request.data.get("code")
             access_token = requests.post(
-                f"https://github.com/login/oauth/access_token?code={code}&client_id=40f6203865331fbc02bc&client_secret={settings.GH_SECRET}",
+                f"https://github.com/login/oauth/access_token?code={code}&client_id=f326b5a02e15ca03d716&client_secret={settings.GH_SECRET}",
                 headers={"Accept": "application/json"},
             )
             access_token = access_token.json().get("access_token")
